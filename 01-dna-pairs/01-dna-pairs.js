a907d77c8fa871f08e11e0602c5c2e6a3d6f6355
amparoamparo <amparozeballos@gmail.com>
2023-02-22 15:18:56 +0000

    add solution to dna-pairs

--- 01-dna-pairs/01-dna-pairs.js
+++ 01-dna-pairs/01-dna-pairs.js
@@ -1,6 +1,50 @@
+/*
+
+Create a function that takes a string of DNA and matches each base with its pair, returning a nested array. In DNA, C pairs with G, and T pairs with A.
+
+Examples:
+
+dnaPair("G");
+should return --> [ ["G", "C"] ]
+
+dnaPair("AG");
+should return --> [ ["A", "T"], ["G", "C"] ]
+
+dnaPair("ATAG");
+should return --> [ ["A", "T"], ["T", "A"], ["A", "T"], ["G", "C"] ]
+
+*/
+
 // Please do not change the name of this function
 function dnaPairs(dna) {
-  // Write your code here
-}
+  dnaPairsArr = [];
+
+  // if the dna string is empty, or if it has characters other than the possible ones, return an empty array
+  if (!dna.length || /[^agtc]/gi.test(dna)) return dnaPairsArr;
+
+  // create variables to store pairs
+  gPair = ["G", "C"];
+  cPair = ["C", "G"];
+  aPair = ["A", "T"];
+  tPair = ["T", "A"];
+
+  // turn dna string into an array using split("")
+  const dnaArr = dna.toUpperCase().split("");
+
+  // use forEach() to iterate through array and add pairs to final array
+  dnaArr.forEach((letter) => {
+    if (letter === "G") {
+      dnaPairsArr.push(gPair);
+    } else if (letter === "A") {
+      dnaPairsArr.push(aPair);
+    } else if (letter === "T") {
+      dnaPairsArr.push(tPair);
+    } else if (letter === "C") {
+      dnaPairsArr.push(cPair);
+    }
+  });
+  
+  return dnaPairsArr;
+};
 
 module.exports = dnaPairs;
--- 01-dna-pairs/__tests__/01-dna-pairs.test.js
+++ 01-dna-pairs/__tests__/01-dna-pairs.test.js
@@ -1,7 +1,48 @@
-const dnaPairs = require('../dna-pairs.js');
+const dnaPairs = require('../01-dna-pairs');
 
-describe('dnaPairs()', () => {
-  test('ADD YOUR TEST DESCRIPTION HERE', () => {
-    // add your test assertion(s) here
+describe('dnaPairs', () => {
+  test('when passed an empty string, return an empty array', () => {
+    expect(dnaPairs("")).toEqual([]);
+  });
+
+  test('when passed the string "G", return a nested array of "G" and "C"', () => {
+    expect(dnaPairs("G")).toEqual([["G", "C"]]);
+  });
+
+  test('when passed the string "A", return a nested array of "A" and "T"', () => {
+    expect(dnaPairs("A")).toEqual([["A", "T"]]);
+  });
+
+  test('when passed the string "T", return a nested array of "T" and "A"', () => {
+    expect(dnaPairs("T")).toEqual([["T", "A"]]);
+  });
+
+  test('when passed the string "C", return a nested array of "C" and "G"', () => {
+    expect(dnaPairs("C")).toEqual([["C", "G"]]);
+  });
+
+  test('when passed the string "AG", return a nested array of "A" and "T", and "G" and "C"', () => {
+    expect(dnaPairs("AG")).toEqual([["A", "T"], ["G", "C"]]);
+  });
+
+  test('when passed a multiple char string with a combination of "A", "G", "T" or "C", return a nested array of their pairs', () => {
+    expect(dnaPairs("GA")).toEqual([["G", "C"], ["A", "T"]]);
+    expect(dnaPairs("TC")).toEqual([["T", "A"], ["C", "G"]]);
+    expect(dnaPairs("ATC")).toEqual([["A", "T"], ["T", "A"], ["C", "G"]]);
+    expect(dnaPairs("ATAG")).toEqual([["A", "T"], ["T", "A"], ["A", "T"], ["G", "C"]]);
+  });
+
+  test('when passed any characters or combination of characters other than "A", "G", "T" or "C", return an empty array', () => {
+    expect(dnaPairs("123")).toEqual([]);
+    expect(dnaPairs("BDE")).toEqual([]);
+    expect(dnaPairs("hello world")).toEqual([]);
+    expect(dnaPairs("HOLA")).toEqual([]);
+    expect(dnaPairs("Hola, ¿cómo estás?")).toEqual([]);
+    expect(dnaPairs("H")).toEqual([]);
+    expect(dnaPairs("z")).toEqual([]);
+  });
+
+  test('return a nested array of corresponding pairs, regardless of case', () => {
+    expect(dnaPairs("ag")).toEqual([["A", "T"], ["G", "C"]]);
   });
 });
