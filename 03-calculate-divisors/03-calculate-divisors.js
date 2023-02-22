2f3a7b5c5a5339a0c6555db4a5a38bff26511e55
amparoamparo <amparozeballos@gmail.com>
2023-02-22 17:45:37 +0000

    add solution to calculate-divisors

--- 03-calculate-divisors/03-calculate-divisors.js
+++ 03-calculate-divisors/03-calculate-divisors.js
@@ -4,10 +4,10 @@ Add FizzBuzz multiples below a certain number
 
 The challenge is to implement a function which adds all the multiples of three and five below a certain number
 
-If we list all the numbers below 12 that are multiples of 3 or 5, we get 3, 5, 6, 9 and 10. The sum of these multiples is 33.
-
 Examples
 --------
+
+If we list all the numbers below 12 that are multiples of 3 or 5, we get 3, 5, 6, 9 and 10. The sum of these multiples is 33.
 
 calculateDivisors(1);
 should return 0
@@ -29,6 +29,16 @@ should return 33
 // Please do not change the name of this function
 function calculateDivisors(num) {
 
+  let sum = 0;
+
+  // iterate through all numbers up to num, find out if there are any multiples of 3 or 5, and add them to sum
+  for (let i = 0; i < num; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i;
+    }
+  }
+  
+  return sum;
 }
 
 module.exports = calculateDivisors;
--- __tests__/03-calculate-divisors.test.js
+++ __tests__/03-calculate-divisors.test.js
@@ -1,7 +1,28 @@
 const calculateDivisors = require('../03-calculate-divisors/03-calculate-divisors');
 
 describe('calculateDivisors', () => {
-  test('', () => {
-    expect(calculateDivisors()).toEqual();
+  
+  test('when passed 0, return 0', () => {
+    expect(calculateDivisors(0)).toBe(0);
+  });
+
+  test('when passed 1, return 0', () => {
+    expect(calculateDivisors(1)).toBe(0);
+  });
+
+  test('when passed 5, return 3', () => {
+    expect(calculateDivisors(5)).toBe(3);
+  });
+
+  test('when passed 6, return 8', () => {
+    expect(calculateDivisors(6)).toBe(8);
+  });
+
+  test('when passed 10, return 23', () => {
+    expect(calculateDivisors(10)).toBe(23);
+  });
+
+  test('when passed 12, return 33', () => {
+    expect(calculateDivisors(12)).toBe(33);
   });
 });
