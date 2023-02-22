a66b2f3d6f71bc4a9864f5488698d72e173faa37
amparoamparo <amparozeballos@gmail.com>
2023-02-22 16:35:55 +0000

    setup files for calculate-divisors

--- 03-calculate-divisors/03-calculate-divisors.js
+++ 03-calculate-divisors/03-calculate-divisors.js
@@ -1,4 +1,34 @@
+/*
+
+Add FizzBuzz multiples below a certain number
+
+The challenge is to implement a function which adds all the multiples of three and five below a certain number
+
+If we list all the numbers below 12 that are multiples of 3 or 5, we get 3, 5, 6, 9 and 10. The sum of these multiples is 33.
+
+Examples
+--------
+
+calculateDivisors(1);
+should return 0
+
+calculateDivisors(5);
+should return 3
+
+calculateDivisors(6);
+should return 8
+
+calculateDivisors(10);
+should return 23
+
+calculateDivisors(12);
+should return 33
+
+*/
+
 // Please do not change the name of this function
 function calculateDivisors(num) {
-  // Your code here
+
 }
+
+module.exports = calculateDivisors;
---
+++ __tests__/03-calculate-divisors.test.js
@@ -0,0 +1,7 @@
+const calculateDivisors = require('../03-calculate-divisors/03-calculate-divisors');
+
+describe('calculateDivisors', () => {
+  test('', () => {
+    expect(calculateDivisors()).toEqual();
+  });
+});
